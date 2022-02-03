61daa15796fc2a0df2b15435afed5dea268269f2
victor <victor@victor.com.au>
2022-02-03 17:28:43 +0100

    Updated filename

--- src/DependencyInjection/AuthenticatorExtension.php
+++ src/DependencyInjection/AuthenticatorExtension.php
@@ -15,6 +15,6 @@ class AuthenticatorExtension extends Extension
             $container,
             new FileLocator(__DIR__.'/../Resources/config')
         );
-        $loader->load('services.xml');
+        $loader->load('services.yaml');
     }
 }
