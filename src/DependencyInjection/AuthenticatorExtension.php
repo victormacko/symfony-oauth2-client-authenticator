719855d05f9c05917cb786e224779c68cd006c77
victor <victor@victor.com.au>
2022-02-03 17:27:43 +0100

    Renamed class

--- src/DependencyInjection/AuthenticatorExtension.php
+++ src/DependencyInjection/AuthenticatorExtension.php
@@ -7,7 +7,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
-class AuthenticatorBundleExtension extends Extension
+class AuthenticatorExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
